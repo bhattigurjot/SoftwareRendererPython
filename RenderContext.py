228b26665407c89cff80718dfa8dfbdf58cb95af
bhattigurjot <bhattigurjot@gmail.com>
2018-11-12 10:04:42 -0600

    Implemented new rendercontext to display line or triangle
    scanline algo implemented

---
+++ RenderContext.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pygame
+
+from Vertex import Vertex
+
+
+class RenderContext:
+    def __init__(self, texture, width, height):
+        self.scanBuffer = np.zeros(height*2)
+        self.texture = texture
+
+    def draw_scanbuffer(self, yCoordinate, xMin, xMax):
+        self.scanBuffer[yCoordinate*2] = xMin
+        self.scanBuffer[yCoordinate*2 + 1] = xMax
+
+    def fill_shape(self, yMin, yMax):
+        for j in range(yMin, yMax):
+            xMin = (int)(self.scanBuffer[j*2])
+            xMax = (int)(self.scanBuffer[j*2+1])
+
+            for i in range(xMin, xMax):
+                self.texture.set_at((i,j), pygame.Color(255,255,255))
+
+    def fill_triangle(self, v1, v2, v3):
+        minYVert = v1
+        midYVert = v2
+        maxYVert = v3
+
+        if maxYVert.get_y() < midYVert.get_y():
+            maxYVert, midYVert = midYVert, maxYVert
+
+        if midYVert.get_y() < minYVert.get_y():
+            midYVert, minYVert = minYVert, midYVert
+
+        if maxYVert.get_y() < midYVert.get_y():
+            maxYVert, midYVert = midYVert, maxYVert
+
+        area = minYVert.triangle_area(maxYVert, midYVert)
+        handedness = 1 if (area >= 0) else 0
+
+        self.scan_convert_triangle(minYVert, midYVert, maxYVert, handedness)
+        self.fill_shape(100, 300)
+
+    def scan_convert_triangle(self, minYVert, midYVert, maxYVert, handedness):
+        self.scan_convert_line(minYVert, maxYVert, 0 + handedness)
+        self.scan_convert_line(minYVert, midYVert, 1 - handedness)
+        self.scan_convert_line(midYVert, maxYVert, 1 - handedness)
+
+    def scan_convert_line(self, minYVert, maxYVert, whichSide):
+        yStart = (int)(minYVert.get_y())
+        yEnd = (int)(maxYVert.get_y())
+        xStart = (int)(minYVert.get_x())
+        xEnd = (int)(maxYVert.get_x())
+
+        yDist = yEnd - yStart
+        xDist = xEnd - xStart
+
+        if yDist <= 0:
+            return 0
+
+        xStep = xDist/yDist
+        currX = xStart
+
+        for j in range(yStart, yEnd):
+            self.scanBuffer[j*2 + whichSide] = (int)(currX)
+            currX += xStep
--- main.py
+++ main.py
@@ -2,15 +2,24 @@
 
 from Display import Display
 from Star3D import Star3D
-
+from RenderContext import RenderContext
+from Vertex import Vertex
 
 def main():
     display = Display(600, 400, "Software Renderer")
     stars = Star3D(1000, 64.0, 20.0)
+    shape = RenderContext(display.get_target(),600,400)
+
+    minYVert = Vertex(100,100)
+    midYVert = Vertex(150,200)
+    maxYVert = Vertex(80,300)
 
     while display.is_running():
         display.poll_events()
-        stars.update_and_render(display.get_target(), display.get_delta())
+        # stars.update_and_render(display.get_target(), display.get_delta())
+
+        shape.fill_triangle(midYVert, maxYVert, minYVert)
+
         display.render()
 
     display.clean_up()
