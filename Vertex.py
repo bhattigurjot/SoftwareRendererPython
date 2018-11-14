294a07a7081336643cf35f6d0209172aa7f8f1b0
bhattigurjot <bhattigurjot@gmail.com>
2018-11-14 10:28:44 -0600

    Added z element to vertex and perspective divide for realistic view

--- Vertex.py
+++ Vertex.py
@@ -2,11 +2,11 @@ from Vector4f import Vector4f
 
 
 class Vertex:
-    def __init__(self, x, y=None):
+    def __init__(self, x, y=None, z=None):
         if y is None:
             self.pos = x
         else:
-            self.pos = Vector4f(x, y, 0, 1)
+            self.pos = Vector4f(x, y, z, 1)
 
     def set_x(self, x):
         self.pos.x = x
@@ -14,14 +14,23 @@ class Vertex:
     def set_y(self, y):
         self.pos.y = y
 
+    def set_z(self, z):
+        self.pos.z = z
+
     def get_x(self):
         return self.pos.x
 
     def get_y(self):
         return self.pos.y
 
+    def get_z(self):
+        return self.pos.z
+
     def transform(self, mat):
         return Vertex(mat.transform(self.pos))
+
+    def perspective_divide(self):
+        return Vertex(Vector4f(self.pos.x/self.pos.w, self.pos.y/self.pos.w, self.pos.z/self.pos.w, self.pos.w))
 
     def triangle_area2times(self, b, c):
         x1 = b.get_x() - self.pos.x
