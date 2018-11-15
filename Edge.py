1391654b1673430068e507fd6f32b00183a17bb1
bhattigurjot <bhattigurjot@gmail.com>
2018-11-15 11:04:30 -0600

    Added edge class and implemented edge scanning

---
+++ Edge.py
@@ -0,0 +1,32 @@
+import math
+
+class Edge:
+    def __init__(self, minYVert, maxYVert):
+        self.yStart = int(math.ceil(minYVert.get_y()))
+        self.yEnd = int(math.ceil(maxYVert.get_y()))
+        self.xStart = int(math.ceil(minYVert.get_x()))
+        self.xEnd = int(math.ceil(maxYVert.get_x()))
+        self.xStep = 0
+
+        yDist = maxYVert.get_y() - minYVert.get_y()
+        xDist = maxYVert.get_x() - minYVert.get_x()
+
+        yPreStep = self.yStart - minYVert.get_y()
+
+        if yDist <= 0.0:
+            self.xStep = self.xStep
+        else:
+            self.xStep = xDist/yDist
+        self.x = self.xStart + yPreStep * self.xStep
+
+    def step(self):
+        self.x += self.xStep
+
+    def get_x(self):
+        return self.x
+
+    def get_y_start(self):
+        return int(self.yStart)
+
+    def get_y_end(self):
+        return int(self.yEnd)
--- RenderContext.py
+++ RenderContext.py
@@ -3,27 +3,28 @@ import math
 import pygame
 
 from Vertex import Vertex
+from Edge import Edge
 from EngineMaths import Matrix4f
 
 
 class RenderContext:
     def __init__(self, texture, width, height):
-        self.scanBuffer = np.zeros(height*2)
+        # self.scanBuffer = np.zeros(height*2)
         self.texture = texture
         self.width = width
         self.height = height
 
-    def draw_scanbuffer(self, yCoordinate, xMin, xMax):
-        self.scanBuffer[yCoordinate*2] = xMin
-        self.scanBuffer[yCoordinate*2 + 1] = xMax
+    # def draw_scanbuffer(self, yCoordinate, xMin, xMax):
+    #     self.scanBuffer[yCoordinate*2] = xMin
+    #     self.scanBuffer[yCoordinate*2 + 1] = xMax
 
-    def fill_shape(self, yMin, yMax):
-        for j in range(yMin, yMax):
-            xMin = (int)(self.scanBuffer[j*2])
-            xMax = (int)(self.scanBuffer[j*2+1])
-
-            for i in range(xMin, xMax):
-                self.texture.set_at((i,j), pygame.Color(255,255,255))
+    # def fill_shape(self, yMin, yMax):
+    #     for j in range(yMin, yMax):
+    #         xMin = (int)(self.scanBuffer[j*2])
+    #         xMax = (int)(self.scanBuffer[j*2+1])
+    #
+    #         for i in range(xMin, xMax):
+    #             self.texture.set_at((i,j), pygame.Color(255,255,255))
 
     def fill_triangle(self, v1, v2, v3):
         screenspace_transform_mat = Matrix4f().init_screenspace_transform(self.width/2,self.height/2)
@@ -41,33 +42,67 @@ class RenderContext:
             maxYVert, midYVert = midYVert, maxYVert
 
         area = minYVert.triangle_area2times(maxYVert, midYVert)
-        handedness = 1 if (area >= 0) else 0
+        # handedness = 1 if (area >= 0) else 0
+        #
+        # self.scan_convert_triangle(minYVert, midYVert, maxYVert, handedness)
+        # self.fill_shape((int)(math.ceil(minYVert.get_y())),
+        #                 (int)(math.ceil(maxYVert.get_y())))
 
-        self.scan_convert_triangle(minYVert, midYVert, maxYVert, handedness)
-        self.fill_shape((int)(math.ceil(minYVert.get_y())),
-                        (int)(math.ceil(maxYVert.get_y())))
+        handedness = (area >= 0)
+        self.scan_triangle(minYVert, midYVert, maxYVert, handedness)
 
-    def scan_convert_triangle(self, minYVert, midYVert, maxYVert, handedness):
-        self.scan_convert_line(minYVert, maxYVert, 0 + handedness)
-        self.scan_convert_line(minYVert, midYVert, 1 - handedness)
-        self.scan_convert_line(midYVert, maxYVert, 1 - handedness)
+    def scan_triangle(self, minYVert, midYVert, maxYVert, handedness):
+        edge_top_to_bottom = Edge(minYVert, maxYVert)
+        edge_top_to_middle = Edge(minYVert, midYVert)
+        edge_middle_to_bottom = Edge(midYVert, maxYVert)
 
-    def scan_convert_line(self, minYVert, maxYVert, whichSide):
-        yStart = math.ceil(minYVert.get_y())
-        yEnd = math.ceil(maxYVert.get_y())
-        xStart = math.ceil(minYVert.get_x())
-        xEnd = math.ceil(maxYVert.get_x())
+        self.scan_edges(edge_top_to_bottom, edge_top_to_middle, handedness)
+        self.scan_edges(edge_top_to_bottom, edge_middle_to_bottom, handedness)
 
-        yDist = yEnd - yStart
-        xDist = xEnd - xStart
+    def scan_edges(self, a, b, handedness):
+        left = a
+        right = b
 
-        if yDist <= 0.0:
-            return 0
+        if handedness:
+            left, right = right, left
 
-        xStep = xDist/yDist
-        yPreStep = yStart - minYVert.get_y()
-        currX = xStart + yPreStep * xStep
+        yStart = b.get_y_start()
+        yEnd = b.get_y_end()
 
         for j in range(yStart, yEnd):
-            self.scanBuffer[j*2 + whichSide] = (int)(math.ceil(currX))
-            currX += xStep
+            self.draw_scanline(left, right, j)
+            left.step()
+            right.step()
+
+    def draw_scanline(self, left, right, j):
+        xMin = (int)(left.get_x())
+        xMax = (int)(right.get_x())
+
+        for i in range(xMin, xMax):
+            self.texture.set_at((i,j), pygame.Color(255,255,255))
+
+    # def scan_convert_triangle(self, minYVert, midYVert,
+    #                           maxYVert, handedness):
+    #     self.scan_convert_line(minYVert, maxYVert, 0 + handedness)
+    #     self.scan_convert_line(minYVert, midYVert, 1 - handedness)
+    #     self.scan_convert_line(midYVert, maxYVert, 1 - handedness)
+    #
+    # def scan_convert_line(self, minYVert, maxYVert, whichSide):
+    #     yStart = math.ceil(minYVert.get_y())
+    #     yEnd = math.ceil(maxYVert.get_y())
+    #     xStart = math.ceil(minYVert.get_x())
+    #     xEnd = math.ceil(maxYVert.get_x())
+    #
+    #     yDist = yEnd - yStart
+    #     xDist = xEnd - xStart
+    #
+    #     if yDist <= 0.0:
+    #         return 0
+    #
+    #     xStep = xDist/yDist
+    #     yPreStep = yStart - minYVert.get_y()
+    #     currX = xStart + yPreStep * xStep
+    #
+    #     for j in range(yStart, yEnd):
+    #         self.scanBuffer[j*2 + whichSide] = (int)(math.ceil(currX))
+    #         currX += xStep
